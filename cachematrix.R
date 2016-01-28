f2497ba6597c7de7d7e7e285f5052f0bf78dae9e
JakubGazda <jkgazda@gmail.com>
2016-01-28 22:40:38 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix() is used to set a matrix, get this matrix
+##along with setting inverse of the matrix and getting inverse of the matrix
+##all possible from the "outside" enviroment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {  ##Setting matrix x from outside of the function enviroment via y
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x  ##Getting matrix x from outside enviroment of the function
+        setinverse<- function(inverse) inv<<-inverse ##Setting inversion of matrix from outside enviroment of the function
+        getinverse <- function() inv ##Getting inversion of matrix from outside enviroment of the function
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+        
 }
 
+##cacheSolve() finds out if an inversion of the matrix has already been done
+## if it has been already done, the function loads the data from cache (the data is saved in makeCacheMatrix())
+##otherwise is computes the inverse and saves it to cache
+## the data is saved to makeCacheMatrix() so the next time it doesnt have to be computed again
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { 
+        if (!is.null(z$getinverse())) { ##checking if the inverse has not been already computed
+                message("getting cached data")
+                return(z$getinverse()) ##if the inverse has already been computed, function loads it from the cache
+        }
+        
+        inverse <- solve(z$get()) ##if the inverse has not been computed yet, it computes it
+        z$setinverse(inverse) ##and saves it to cache
+        return(inverse)
 }
